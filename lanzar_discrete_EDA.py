1fac1e70d5e0e9e9042489a4e779934a5182c126
rsantana-isg <roberto.santana@ehu.es>
2026-01-03 16:55:48 +0100

    Added batch script and cluster launch files

---
+++ lanzar_discrete_EDA.py
@@ -0,0 +1,17 @@
+import sys
+import os
+import numpy as np
+
+n_gen = 50
+n = 30 
+p_size = 150
+ 
+if __name__ == '__main__':        
+	#obj_functions = ['OneMax', 'KDeceptive3', 'Deceptive3', 'HIFF', 'KDeceptive5', 'FC5']
+        obj_functions = ['OneMax']
+        NN_EDAs = ['VAE','GAN', 'Backdrive', 'DAE', 'RBM', 'DbD','UMDA', 'TreeEDA', 'EBNA', 'MOA','MN-FDA', 'MN-FDAG', 'MK-EDA1', 'MK-EDA2', 'MK-EDA3','MT-EDA2', 'MT-EDA3']
+        for seed in np.arange(1,2):
+            for alg in NN_EDAs:
+                for i in range(len(obj_functions)):
+                    A = "sbatch slurm_pateda.sh examples/discrete_EDA.py " +str(seed)+" "+obj_functions[i]+" "+str(n)+" "+str(p_size)+" "+str(n_gen) +" "+alg
+                    print(A)
---
+++ slurm_pateda.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Job name
+#SBATCH --job-name=discrete_eda
+
+# Define the files which will contain the Standard and Error output
+#SBATCH --output=outputs/M_%A_%a.out
+#SBATCH --error=outputs/M_%A_%a.err
+
+# Number of tasks that compose the job
+#SBATCH --ntasks=1
+
+# Advanced use
+# #SBATCH --cpus-per-task=20
+# #SBATCH --threads-per-core=2
+# #SBATCH --ntasks-per-core=2
+
+# Required memory (Default 2GB)
+# SBATCH --mem-per-cpu=8G
+
+# Select one partition
+# GPU
+# SBATCH --partition=GPU
+## SBATCH --gpus=1
+
+
+# If you are using arrays, specify the number of tasks in the array
+## SBATCH --array=1-1
+
+
+
+echo   "bnd -exec python3 $1 $2 $3 $4 $5 $6 $7 > results_discrete_eda_$3_$4_$5_$6_$7_$2.dat"        
+        bnd -exec python3 $1 $2 $3 $4 $5 $6 $7 > results_discrete_eda_$3_$4_$5_$6_$7_$2.dat
+
+
+# sbatch examples/slurm_pateda.sh discrete_eda.py 111 OneMax 20 80 20 VAE
